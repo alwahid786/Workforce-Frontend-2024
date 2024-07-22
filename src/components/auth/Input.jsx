ccf70cd013476390f78a838453e37d38e1667542
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-07-22 16:49:43 +0500

    Add prop-types dependencies

--- src/components/auth/Input.jsx
+++ src/components/auth/Input.jsx
@@ -1,19 +1,38 @@
+import PropTypes from 'prop-types';
 
-const Input = ({ label, type, id, change, click, name , placeholder}) => {
+const Input = ({ label, type, id, change, click, name, placeholder, error }) => {
   return (
-    <>
-      <label className="text-[#000] text-base mb-4 md:mb-6 block">{label}</label>
+    <div className="mb-4">
+      <label className="text-[#000] text-base mb-2 block">{label}</label>
       <input
         onChange={change}
         onClick={click}
         type={type}
         id={id}
         name={name}
-        className="bg-[#f7fbfe] border border-[#e2e5ff] rounded-[14px] w-full h-[60px] md:h-[80px] focus:outline-none px-4"
+        className={`bg-[#f7fbfe] border ${error ? 'border-red-500' : 'border-[#e2e5ff]'} rounded-[14px] w-full h-[60px] md:h-[80px] focus:outline-none px-4`}
         placeholder={placeholder}
       />
-    </>
+      {error && <span className="text-red-500 text-sm mt-1 block">{error}</span>}
+    </div>
   );
+};
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  change: PropTypes.func.isRequired,
+  click: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  error: PropTypes.string,
+};
+
+Input.defaultProps = {
+  click: () => {},
+  placeholder: '',
+  error: '',
 };
 
 export default Input;
